a731dc760270d9f1ace42f9cdf9a45bb37a70b1d
NadaHamdyMegahed <nada.ahmedsaleh@student.guc.edu.eg>
2020-06-10 01:48:57 +0200

    prolog and chr

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const fs = require('fs');
 app.use(express.json());
+var cmd=require('node-cmd');
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -19,6 +20,8 @@ app.use(
     exposedHeaders: "X-Auth-Token"
   })
 );
+
+//////nadas bachelor///////
 
 
 //load code to prolog file
@@ -40,13 +43,13 @@ if (err) {
 //execute query and save result in txt file
 app.post('/execute/',(req,res)=>{
 
-  fs.writeFile('./queryResult.txt', "The query is not executed try again", err => {
+  fs.writeFile('./prologFiles/queryResult.txt', "The query is not executed try again", err => {
     if (err) {
       console.error(err)
       return
     }
     })
-  var cmd=require('node-cmd');
+ 
 
 const processRef=cmd.get('swipl prologFiles/main.pl');
 let data_line = '';
@@ -67,7 +70,7 @@ processRef.stdout.on(
     data_line += data;
     if (x.length > 1) {
    
-      fs.writeFile('./queryResult.txt', data_line, function (err,data) {
+      fs.writeFile('./prologFiles/queryResult.txt', data_line, function (err,data) {
         if (err) {
           return console.log(err);
         }
@@ -99,15 +102,115 @@ res.send("done");
 //get result of query
 app.get('/getRes/',(req,res)=>{
 
-  fs.readFile("queryResult.txt", function(err, buf) {
+  fs.readFile("prologFiles/queryResult.txt", function(err, buf) {
     if(err) res.send("error",err);
     else{
     res.send(buf.toString().replace('Content-type: text/html; charset=UTF-8', ''));
     }
   });
-
  
 })
+
+//////kimoos///
+
+app.post('/loadCHR/',(req,res)=>{
+  var code = req.body.code;
+  
+  fs.writeFile('chrFiles/main.pl', code, err => {
+  if (err) {
+    console.error(err)
+    return
+  }
+  })
+      
+    res.send(code)
+   
+  })
+  
+  
+  //execute query and save result in txt file
+  app.post('/executeCHR/',(req,res)=>{
+  
+    fs.writeFile('./chrFiles/queryResult.txt', "The query is not executed try again", err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+      })
+   
+  
+  const processRef=cmd.get('swipl chrFiles/main.pl');
+  let data_line = '';
+  
+  var query = req.body.query+"\n";
+  
+  var or=`;
+  `;
+  var and=`.
+  `;
+  
+  processRef.stdin.write(query);
+  
+  processRef.stdout.on(
+    'data',
+    function(data) {
+      var x = data.split("\n")
+      data_line += data;
+      if (x.length > 1) {
+     
+        fs.writeFile('./chrFiles/queryResult.txt', data_line, function (err,data) {
+          if (err) {
+            return console.log(err);
+          }
+          
+          
+        }
+        );
+    } 
+     else{
+       if(data_line.length<=100000){
+  processRef.stdin.write(or);
+  
+       }
+       else{
+         console.log("exeeds limit")
+  
+        processRef.stdin.write(and);
+        alert("this is only part of the solutions as there are too many possible solutions !!")
+          
+       }
+      }
+    }
+  );
+  
+  res.send("done");
+     
+  })
+  
+  //get result of query
+  app.get('/getCHRRes/',(req,res)=>{
+  
+    fs.readFile("chrFiles/queryResult.txt", function(err, buf) {
+      if(err) res.send("error",err);
+      else{
+      res.send(buf.toString().replace('Content-type: text/html; charset=UTF-8', ''));
+      }
+    });
+   
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.listen(8000,()=>{ console.log("listening on 8000") });
 
--- prologFiles/Code.pl
+++ prologFiles/Code.pl
@@ -1,8 +1,14 @@
 :-module(Code,[]).
-
 insert(A,[B|C],[B|D]):-A@>B,!,insert(A,C,D).
 insert(A,C,[A|C]).
 sorting([A|B],S):-sorting(B,ST),insert(A,ST,S),visualize(S).
 sorting([],[]).
-nada(nada).
-nada(sara).
+
+tran([],0).
+tran([X],0).
+tran([X,Y|T],N2):-
+X<Y,tran([Y|T],N1),
+N2 is N1+1.
+tran([X,Y|T],N2):-
+Y=<X,tran([Y|T],N2).
+
